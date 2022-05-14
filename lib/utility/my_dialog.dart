00f8a60f97ff6cea35aac080e8ff076aa3f24b5f
numnim <rawangcha5song@gmail.com>
2022-05-14 23:05:04 +0700

    finish 14 may

--- lib/utility/my_dialog.dart
+++ lib/utility/my_dialog.dart
@@ -40,7 +40,7 @@ class MyDialog {
               ? const SizedBox()
               : ShowTextButton(label: label2!, pressFunc: pressFunc2),
           ShowTextButton(
-              label: 'cancael',
+              label: pressFunc1 == null ? 'OK' : 'cancael' ,
               pressFunc: () {
                 Navigator.pop(context);
               }),

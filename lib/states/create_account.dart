3439035d4947447973f88bc15e4fe4eae73a6403
numnim <rawangcha5song@gmail.com>
2022-05-14 10:37:31 +0700

    Design UI Succes

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:numnimsocial/utility/my_constant.dart';
+import 'package:numnimsocial/utility/my_dialog.dart';
 import 'package:numnimsocial/widgets/show_button.dart';
 import 'package:numnimsocial/widgets/show_form.dart';
 import 'package:numnimsocial/widgets/show_icon_button.dart';
@@ -17,7 +18,7 @@ class CreateAccount extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          newWidget(widget: newAvatar()),
+          newWidget(widget: newAvatar(context: context)),
           newWidget(
               widget: ShowForm(label: 'Name :', iconData: Icons.fingerprint)),
           newWidget(
@@ -46,21 +47,35 @@ class CreateAccount extends StatelessWidget {
     );
   }
 
-  Container newAvatar() {
+  Container newAvatar({required BuildContext context}) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 32),
       width: 250,
       height: 250,
       child: Stack(
         children: [
-          Positioned(bottom: 0,right: 0,
-            child: ShowIconButton(
-              iconData: Icons.add_a_photo,
-              pressFunc: () {},
-            ),
-          ),
           const ShowImage(
             path: 'images/avatar.png',
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: ShowIconButton(
+              iconData: Icons.add_a_photo,
+              pressFunc: () {
+                MyDialog(context: context).twoWayAction(
+                    pressFunc2: () {
+                      Navigator.pop(context);
+                    },
+                    label2: 'Gallery',
+                    pressFunc1: () {
+                      Navigator.pop(context);
+                    },
+                    label1: 'camera',
+                    title: 'Require Photo',
+                    subTitle: 'Please Tap Camera or Gallery');
+              },
+            ),
           ),
         ],
       ),
---
+++ lib/widgets/show_listtile.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:numnimsocial/utility/my_constant.dart';
+import 'package:numnimsocial/widgets/show_text.dart';
+
+class ShowListtile extends StatelessWidget {
+  final String title;
+  final Widget widget;
+  const ShowListtile({
+    Key? key,
+    required this.title,
+    required this.widget,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        leading: widget,
+        title: ShowText(
+          label: title,
+          textStyle: MyConstant().h2Style(),
+        ));
+  }
+} // End Class
